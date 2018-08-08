27ecf3f6610b086716f97b6dbe272a676e52ba1d
Aleksandr <impressive-art@mail.ru>
2018-08-08 15:29:59 +0300

    DoneTest

--- TheBestWar/Assets/Scripts/_Dialog/DialogManager.cs
+++ TheBestWar/Assets/Scripts/_Dialog/DialogManager.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class DialogManager : MonoBehaviour {
+	public Text nameText;
+	public Text dialogText;
+
 	Queue<string> sentences;
 
 	void Start () {
@@ -10,6 +14,38 @@ public class DialogManager : MonoBehaviour {
 	}
 
 	public void StartDialog (Dialog dialog) {
-		Debug.Log ("Начался диалог с " + dialog.name);
+//		Debug.Log ("Начался диалог с " + dialog.name);
+
+		nameText.text = dialog.name;
+		sentences.Clear ();
+
+		foreach (string sentence in dialog.sentences) {
+			sentences.Enqueue (sentence);
+		}
+		DisplayNextSentence ();
+	}
+
+	public void DisplayNextSentence () {
+		if (sentences.Count == 0) {
+			EndDialog ();
+			return;
+		}
+		string sentence = sentences.Dequeue ();
+		StopAllCoroutines ();
+		StartCoroutine (TypeSentence (sentence));		//отвечает за вывод букв диалога
+
+//		Debug.Log (sentence);
+	}
+
+	IEnumerator TypeSentence (string sentence) {
+		dialogText.text = "";
+		foreach (char letter in sentence.ToCharArray()) {
+			dialogText.text += letter;
+			yield return null;
+		}
+	}
+
+	public void EndDialog () {
+		Debug.Log ("Stop conversation");
 	}
 }
